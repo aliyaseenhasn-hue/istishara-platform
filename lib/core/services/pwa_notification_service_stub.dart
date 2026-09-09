b4e23edfd7b2cac5da43b6b160955aecd9319e56
aliyaseenhasn-hue <aliyaseenhasn@gmail.com>
2026-09-10 01:57:16 +0300

    fix: add PWA push ownership lifecycle hooks

--- lib/core/services/pwa_notification_service_stub.dart
+++ lib/core/services/pwa_notification_service_stub.dart
@@ -1,9 +1,15 @@
 class PwaNotificationService {
   static bool get supported => false;
 
+  static Future<void> initialize() async {}
+
   static Future<bool> isEnabled() async => false;
 
   static Future<bool> enable() async => false;
+
+  static Future<bool> syncForCurrentUser() async => false;
+
+  static Future<void> releaseForCurrentUser() async {}
 
   static Future<bool> disable() async => true;
 }
